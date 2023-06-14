124b8ce0fbb0c0512c074b163081990d868743db
Tom Lewis <tom.lewis488@gmail.com>
2023-06-14 21:28:03 +0200

    Updated main css

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -2,19 +2,14 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background: ${(props) => props.theme.colors.mainBg};
-  padding: 0 2em;
+  padding: 0 2em
   height: 50px;
   color: ${(props) => props.theme.colors.secondary};
-  @media (min-width: 1211px) {
-    display: flex;
-    align-items: center;
     }
   }
 
   .MobileMenuItem {
-    padding-right: 2em;
-  @media (min-width: 1211px) {
-    display: none
+    padding-right: 1em;
     }
   }
 
@@ -40,17 +35,13 @@ export const IconContainer = styled.div`
 `;
 
 export const FlexContainer = styled.div`
-  @media(min-width: 300px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1em;
   }
 `;
 
 export const LogoContainer = styled.div`
-  @media(max-width: 1210px) {
-    padding-right: 150px;
-  }
 `;
 
 export const LinkContainer = styled.div`
--- pages/photo.tsx
+++ pages/photo.tsx
@@ -5,6 +5,7 @@ const Photo: NextPage = () => {
   return (
     <Container title="Photo">
       <div>Photo</div>
+      <a data-flickr-embed="false" href="https://www.flickr.com/photos/198535393@N04/52973854993/in/dateposted/" title="IMG_20211016_132149"><img src="https://live.staticflickr.com/65535/52973854993_f57a8c7183_k.jpg" width="1155" height="2048" alt="IMG_20211016_132149"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
     </Container>
   );
 };
