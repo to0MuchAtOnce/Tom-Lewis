01933f5bfa1273805ff5f9fb7c775f6f09b364d9
Tom lewis <tom.lewis488@gmail.com>
2023-06-27 20:58:24 +0200

    Updated syntax highlighting

---
+++ pages/posts/Markdown.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PostContainer = styled.div`
+  p {
+    line-height: 1.5;
+    padding: 5px 0;
+  }
+
+  pre.language-tsx {
+    border-radius: 7px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    margin: 10px 0;
+  }
+
+  h3 {
+  }
+`;
