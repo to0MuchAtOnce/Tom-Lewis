db90bef3115ff81cb1c0f07b29e62ddf40510f2b
Tom Lewis <tom.lewis488@gmail.com>
2023-07-14 11:42:46 +0200

    Updated photo.tsx

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -4,7 +4,10 @@ export default async function handler(req: any, res: any): Promise<void> {
   const params = JSON.parse(req.body);
   // Run search request
   const results = await search(params);
+
   res.status(200).json({
     ...results,
   });
+
+  console.log(results);
 }
