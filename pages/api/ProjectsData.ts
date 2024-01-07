e9099563413e3b6e7d634b705df8f0e5a1ac56e4
Tom lewis <tom.lewis488@gmail.com>
2024-01-07 23:12:46 +0100

    Add new project

--- pages/api/ProjectsData.ts
+++ pages/api/ProjectsData.ts
@@ -8,18 +8,24 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 0,
+    title: 'Calculator',
+    content: 'A simple calucator built with HTML, CSS and Javascript',
+    link: 'https://to0muchatonce.github.io/odin-calculator/',
+  },
+  {
+    id: 1,
     title: 'Etch-a-Sketch',
     content: "A web based version of the popular children's game",
     link: 'https://to0muchatonce.github.io/etch-a-sketch/',
   },
   {
-    id: 1,
+    id: 2,
     title: 'Rock, Paper, Scissors',
     content: 'My version of the popular game',
     link: 'https://to0muchatonce.github.io/rock-paper-scissors/',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Frontend Mentor Challenges',
     content: 'A page of all my frontend mentor submissions',
     link: 'https://vigorous-gates-fc2d75.netlify.app/',
