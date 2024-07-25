1481ee86598f15411d39a0f1e64daa22e2eea864
Tom Lewis <tom.lewis488@gmail.com>
2024-07-25 14:53:52 +0200

    Add new project

--- pages/api/ProjectsData.ts
+++ pages/api/ProjectsData.ts
@@ -8,24 +8,30 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 0,
-    title: 'Calculator',
-    content: 'A simple calucator built with HTML, CSS and Javascript',
-    link: 'https://to0muchatonce.github.io/odin-calculator/',
+    title: 'Library App',
+    content: 'A Library app built with HTML, CSS and Javascript',
+    link: 'https://to0muchatonce.github.io/library-app/',
   },
   {
     id: 1,
+    title: 'Calculator',
+    content: 'A simple calculator built with HTML, CSS and Javascript',
+    link: 'https://to0muchatonce.github.io/odin-calculator/',
+  },
+  {
+    id: 2,
     title: 'Etch-a-Sketch',
     content: "A web based version of the popular children's game",
     link: 'https://to0muchatonce.github.io/etch-a-sketch/',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Rock, Paper, Scissors',
     content: 'My version of the popular game',
     link: 'https://to0muchatonce.github.io/rock-paper-scissors/',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Frontend Mentor Challenges',
     content: 'A page of all my frontend mentor submissions',
     link: 'https://vigorous-gates-fc2d75.netlify.app/',
